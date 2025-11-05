dae4c684607dd581dc93e445874e1f3529ed1860
Чичканов Дмитрий Владимирович <chdmitr@skbkontur.ru>
2025-11-05 09:39:25 +0500

    перенос исходников в отдельную папку

--- gen_cert.sh
+++ gen_cert.sh
@@ -1,4 +1,4 @@
-mkdir -p ./cert
+mkdir -p ./src/cert
 
-openssl req -x509 -newkey rsa:4096 -keyout ./cert/server.key -out ./cert/server.crt -sha256 -days 3650 -nodes \
+openssl req -x509 -newkey rsa:4096 -keyout ./src/cert/server.key -out ./src/cert/server.crt -sha256 -days 3650 -nodes \
     -subj "/C=XX/ST=StateName/L=CityName/O=CompanyName/OU=CompanySectionName/CN=localhost"
